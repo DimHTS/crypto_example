e72ea57a8c2bcc65ad96412a4aad2ae22abe1e82
Dim Hutsel <dreamtm@yandex.ru>
2018-05-23 12:33:12 +0700

    add base configuration in project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux'
+import createBrowserHistory from 'history/createBrowserHistory'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './node_modules/reducers'
+import App from './node_modules/App'
+import registerServiceWorker from './registerServiceWorker'
+
+
+const browserHistory = createBrowserHistory({ basename: '/react/crypto_example' })
+
+const middleware = applyMiddleware(thunk, routerMiddleware(browserHistory))
+const store = createStore(reducer, composeWithDevTools(middleware))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+
+const Root = (
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
+)
+
+ReactDOM.render(Root, document.getElementById('root'));
 registerServiceWorker();
